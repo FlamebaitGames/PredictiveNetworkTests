ce588e2ae60e6a91393284915725254e2854e39d
niklasl@flamebaitgames.com <>
2019-04-25 16:45:20 +0200

    basic movement + clients

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	private new Rigidbody rigidbody;
+	private void Awake()
+	{
+		rigidbody = GetComponent<Rigidbody>();
+	}
+	private void FixedUpdate()
+	{
+		var input = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		input.Normalize();
+		rigidbody.AddForce(input * 150f * Time.fixedDeltaTime);
+		if(rigidbody.position.magnitude > 7f)
+		{
+			rigidbody.AddForce(-rigidbody.position, ForceMode.Acceleration);
+		}
+		gameObject.scene.GetPhysicsScene().Simulate(Time.fixedDeltaTime);
+	}
+}
---
+++ Assets/Server.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Linq;
+
+public class Server : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject playerPrefab;
+	[SerializeField]
+	private Camera playerCamera;
+	private int[] layers;
+	private int nClients;
+	private void Awake()
+	{
+		layers = new[] { 0, 1, 2, 3, 4, 5 }.Select(n => LayerMask.NameToLayer($"Client{n}")).ToArray();
+	}
+	private void Start()
+	{
+		ClientJoin();
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Return)) ClientJoin();
+	}
+	public void ClientJoin()
+	{
+		var scene = SceneManager.CreateScene($"Client Scene ({nClients})", new CreateSceneParameters(LocalPhysicsMode.Physics3D));
+		var player = Instantiate(playerPrefab);
+		SceneManager.MoveGameObjectToScene(player, scene);
+		player.layer = layers[nClients];
+		var camera = Instantiate(playerCamera);
+		SceneManager.MoveGameObjectToScene(camera.gameObject, scene);
+		camera.targetDisplay = nClients;
+		camera.gameObject.layer = layers[nClients];
+		camera.cullingMask = 1 << layers[nClients];
+		nClients++;
+	}
+	
+}
